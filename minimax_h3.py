a376e256d70474316656c319e3c67ce134fcddde
spracknow-droid <spracknow@gmail.com>
2026-08-06 16:20:15 +0900

    Update minimax_h3.py

--- minimax_h3.py
+++ minimax_h3.py
@@ -5,22 +5,22 @@ download_tasks = [
     {
         "repo": "Comfy-Org/MiniMax-H3", 
         "file": "diffusion_models/minimax_h3_fl2va_pruned_int8_convrot.safetensors",  # 디퓨전 모델
-        "dir": "/workspace/runpod-slim/ComfyUI/models/diffusion_models"
+        "dir": "/workspace/runpod-slim/ComfyUI/models"
     },
     {
         "repo": "Comfy-Org/MiniMax-H3", 
         "file": "text_encoders/qwen3vl_32b_minimax_h3_nvfp4_awq.safetensors",  # 텍스트 인코더
-        "dir": "/workspace/runpod-slim/ComfyUI/models/text_encoders"
+        "dir": "/workspace/runpod-slim/ComfyUI/models"
     },
     {
         "repo": "Comfy-Org/MiniMax-H3", 
         "file": "vae/minimax—h3_video_vae_fp16.safetensors",  # 비디오 VAE
-        "dir": "/workspace/runpod-slim/ComfyUI/models/vae"
+        "dir": "/workspace/runpod-slim/ComfyUI/models"
     },
     {
         "repo": "Comfy-Org/MiniMax-H3", 
         "file": "vae/minimax—h3_audeo_vae_fp32.safetensors",   # 오디오 VAE
-        "dir": "/workspace/runpod-slim/ComfyUI/models/vae"
+        "dir": "/workspace/runpod-slim/ComfyUI/models"
     }
 ]
 
